31473588293902d976a33f08948300120be435e8
mahdimorasmmb <67733580+mahdimorasmmb@users.noreply.github.com>
2021-09-26 13:03:37 +0330

    tilwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./assets/tailwind.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="p-10">
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default App;
